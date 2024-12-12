60491d123a70c9eb12e856573547f64b84aa8b8e
maah0060 <matildaahlberg99@gmail.com>
2024-12-12 09:44:17 +0100

    min app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { PostsComponent } from './posts/posts.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, PostsComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'AngularApp';
+  title = 'angularApp';
+  url =
+    'https://raw.githubusercontent.com/maah0060/Data-json/refs/heads/main/data.json';
+  apiData: any = [];
+
+  async getAPIData(): Promise<[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
+  }
 }
